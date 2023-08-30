24cde7b5961824b4c3e0ea78e6509965f76a6cc8
Nanna Ellegaard <nel@adm.ku.dk>
2023-08-30 16:11:38 +0200

    Background image

--- ext_tables.sql
+++ ext_tables.sql
@@ -6,5 +6,7 @@
 -- Table structure for table 'tt_content'
 --
 CREATE TABLE tt_content (
-   tx_ucph_content_bg_color VARCHAR(255) DEFAULT '' NOT NULL
+   tx_ucph_content_bg_color VARCHAR(255) DEFAULT '' NOT NULL,
+   tx_ucph_content_background_image int(11) unsigned DEFAULT '0',
+   tx_ucph_content_background_image_options mediumtext,
 );
