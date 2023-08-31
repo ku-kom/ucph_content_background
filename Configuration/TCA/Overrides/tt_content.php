501a9ab0cbb68945d8f9dc12d94128f29a9c7449
Nanna Ellegaard <nel@adm.ku.dk>
2023-08-31 14:34:37 +0200

    Setup color select

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -10,12 +10,13 @@
 defined('TYPO3') or die();
 
 call_user_func(function ($extKey ='ucph_content_background') {
-
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns(
-        'tt_content',
+    // Register fields
+    $GLOBALS['TCA']['tt_content']['columns'] = array_replace_recursive(
+        $GLOBALS['TCA']['tt_content']['columns'],
         [
             'tx_ucph_content_bg_color' => [
                 'label' => 'LLL:EXT:' . $extKey . '/Resources/Private/Language/locallang_be.xlf:ucph_page_color_select',
+                'onChange' => 'reload',
                 'displayCond' => [
                     'OR' => [
                         // Only add background color select box in these grid CTypes
@@ -65,13 +66,69 @@ call_user_func(function ($extKey ='ucph_content_background') {
                 ],
                 'default' => '',
             ],
-         ]
+            'tx_ucph_content_background_image' => [
+                'exclude' => true,
+                'displayCond' => [
+                    'OR' => [
+                        // Only add background color select box in these grid CTypes
+                        'FIELD:CType:=:ucph_content_container_grids',
+                        'FIELD:CType:=:container_1_columns',
+                        'FIELD:CType:=:container_2_columns',
+                        'FIELD:CType:=:container_2_columns_right',
+                        'FIELD:CType:=:container_2_columns_left',
+                        'FIELD:CType:=:container_3_columns',
+                        'FIELD:CType:=:container_4_columns',
+                    ],
+                    'AND' => [
+                        // If no background color is selected
+                        'FIELD:tx_ucph_content_bg_color:=:',
+                    ],
+                 ],
+                'label' => 'LLL:EXT:' . $extKey . '/Resources/Private/Language/locallang_be.xlf:field.background_image',
+                'config' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::getFileFieldTCAConfig(
+                    'image',
+                    [
+                        'minitems' => 0,
+                        'maxitems' => 1,
+                    ],
+                    $GLOBALS['TYPO3_CONF_VARS']['GFX']['imagefile_ext']
+                ),
+                'l10n_mode' => 'exclude',
+            ],
+            'tx_ucph_content_background_image_options' => [
+                'displayCond' => [
+                    'OR' => [
+                        // Only add background color select box in these grid CTypes
+                        'FIELD:CType:=:ucph_content_container_grids',
+                        'FIELD:CType:=:container_1_columns',
+                        'FIELD:CType:=:container_2_columns',
+                        'FIELD:CType:=:container_2_columns_right',
+                        'FIELD:CType:=:container_2_columns_left',
+                        'FIELD:CType:=:container_3_columns',
+                        'FIELD:CType:=:container_4_columns',
+                    ],
+                    'AND' => [
+                        // If no background color is selected
+                        'FIELD:tx_ucph_content_bg_color:=:',
+                    ],
+                 ],
+                'exclude' => true,
+                'label' => 'LLL:EXT:' . $extKey . '/Resources/Private/Language/locallang_be.xlf:field.background_image_options',
+                'config' => [
+                    'type' => 'flex',
+                    'ds' => [
+                        'default' => 'FILE:EXT:' . $extKey . '/Configuration/FlexForms/BackgroundImage.xml',
+                    ],
+                ],
+                'l10n_mode' => 'exclude',
+            ],
+        ]
     );
 
     // Add in tab "Appearence"
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes(
         'tt_content',
-        'tx_ucph_content_bg_color',
+        'tx_ucph_content_bg_color, tx_ucph_content_background_image, tx_ucph_content_background_image_options',
         '',
         'after:space_after_class'
     );
